1203b69c679bbdb9d1ed2c4e10039b9e4e0c1f1d
Administrator <1>
2018-05-21 14:51:27 +0800

    2

--- config-server/src/main/java/com/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/config/ConfigServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigApplication {
+public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
